042c5e3ccc983512221c863970503ae8a6237622
haru2036 <haru2036@gmail.com>
2014-10-01 17:27:01 +0900

    Add: Some functions for make trigrams of parts

--- Main.hs
+++ Main.hs
@@ -2,4 +2,9 @@ import Text.Nagato.Query.Trigram
 import Text.Nagato.Query.Morphologic
 
 
+makePartsTrigram :: String -> IO [Trigram String]
+makePartsTrigram sentence = do
+  parts <- parseParts sentence
+  return $ makeTrigramList parts
+
 
