fc2395d686774d5ccad2d5a35903df9f0cc83c54
haru2036 <haru2036@gmail.com>
2014-10-11 19:59:39 +0900

    Fix: Using Nagato's action

--- Text/Nagato/Query/Morphologic.hs
+++ Text/Nagato/Query/Morphologic.hs
@@ -7,8 +7,7 @@ import Text.Nagato.MeCabTools
 
 parseParts :: String -> IO [String]
 parseParts sentence = do
-  mecab <- new2 ""
-  parsed <- parse mecab sentence
+  parsed <- parseFormat sentence
   return (map part (init (lines parsed)))
 
 part :: String -> String
